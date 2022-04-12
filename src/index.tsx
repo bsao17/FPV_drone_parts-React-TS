112a384d2ef2f38542e9d10506b81617a9f4a16a
bsao17 <declic62@gmail.com>
2022-04-12 17:56:29 +0200

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
-// import ReactDOM from "react-dom/client";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { render } from '@testing-library/react';
 
-// const container = document.getElementById("root");
-// const root = ReactDOM.createRoot(container as HTMLElement);
-// root.render(<App />);
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container as HTMLElement);
+root.render(<App />);
 
-ReactDOM.render(<App/>, document.getElementById("root"))
 
 reportWebVitals(console.log);
