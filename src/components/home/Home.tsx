298c2049d5d0d3a1c2118ed6df00bbdde930ec60
bsao17 <declic62@gmail.com>
2022-04-12 19:06:04 +0200

    add styled component for dark and day button mode

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,26 +1,43 @@
-import React, {useState, useContext, ReactElement, Context} from "react";
+import React, {useState} from "react";
 import styles from "./home.module.scss";
+import styled from "styled-components";
 
 interface props {
 }
+
+const NightModeButton = styled.button`
+  background-color: #000;
+  border-radius: 5px;
+  border: inset gray 5px;
+`
+const DayModeButton = styled.button`
+  background-color: #fff;
+  border-radius: 5px;
+  border: inset yellow 5px;
+`
 
 const ref: React.RefObject<any> = React.createRef();
 
 
 function Home(props: props) {
-    const[title] = useState<String>("My new Hobby")
-    const[theme, setTheme] = useState(true)
+    const [title] = useState<String>("My new Hobby")
+    const [theme, setTheme] = useState(true)
+
     // const darkTheme = useContext()
 
     function darkMode() {
         setTheme(!theme)
     }
+
     return (
-        <div className={styles.container} >
+        <div className={styles.container}>
             <h1 ref={ref} className={styles.title}>
                 <span>{title}</span>
                 <br/>
-                <button className={theme ? "btn btn-dark" : "btn btn-light"} onClick={darkMode}><h2>{theme ? "🌚" : "🌞"}</h2></button>
+                {theme ? (<NightModeButton className={theme ? "btn btn-dark" : "btn btn-light"} onClick={darkMode}>
+                    <h2>{theme ? "🌚" : "🌞"}</h2></NightModeButton>) : (
+                    <DayModeButton className={theme ? "btn btn-dark" : "btn btn-light"} onClick={darkMode}>
+                        <h2>{theme ? "🌚" : "🌞"}</h2></DayModeButton>)}
             </h1>
         </div>
     );
