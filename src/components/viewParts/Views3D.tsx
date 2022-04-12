fb8d9eb5f421925600cfb2115de8c2f268da3f55
bsao17 <declic62@gmail.com>
2022-04-12 17:28:15 +0200

    delete video support

--- src/components/viewParts/Views3D.tsx
+++ src/components/viewParts/Views3D.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import styles from "./views3D.module.scss"
+import support from "./FPV-support_2022.png"
 
 interface props {}
 
@@ -9,9 +10,10 @@ class Views3D extends React.Component<props> {
   render() {
     return (
       <div>
-        <video id={styles.antenna} controls>
+        {/* <video id={styles.antenna} controls>
           <source src={"./assets/video/Antenna-FPV_2022.mp4"} type={"video/mp4"} />
-        </video>
+        </video> */}
+        <img className={styles.image} src={support} alt="FPV-support" />
       </div>
     );
   }
