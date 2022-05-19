a4ab93936aa9bc6c5e760dbc01ea66ec2213d348
bsao17 <declic62@gmail.com>
2022-05-19 19:03:40 +0200

    update

--- src/components/viewParts/Views3D.tsx
+++ src/components/viewParts/Views3D.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./views3D.module.scss"
 import support from "./images/FPV-support_2022.png"
+import {Link} from "react-router-dom";
 
 class Views3D extends React.Component {
     render() {
@@ -13,9 +14,9 @@ class Views3D extends React.Component {
                     <img id={styles.image} src={support} alt="FPV-support"/>
                     <h3>Support GoPro</h3>
                     <div className={styles.buttons}>
-                        <a className={"btn btn-success me-3"} download={"cura GoPro modele (ver. 1.0.0)"} href={"./fileSTL/FPV-support.stl"}>Donwload</a>
-                        <a className={"btn btn-primary"} href="../../html/Support-FPV_2022.html" target={"_blank"}>3D
-                            view</a>
+                        <a className={"btn btn-success me-3"} download={"cura GoPro modele (ver. 1.0.0)"}
+                           href={"./fileSTL/FPV-support.stl"}>Donwload</a>
+                        <a href={"https://bsao17.github.io/FPV_drone_parts/FPV-support.html"} className={"btn btn-primary"} >3D View</a>
                     </div>
                 </div>
                 <hr id={styles.separation}/>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Context from "./components/context/Context";
 import reportWebVitals from "./reportWebVitals";
+import {BrowserRouter} from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container as HTMLElement);
 root.render(
     <Context>
-        <App/>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </Context>
 );
 
