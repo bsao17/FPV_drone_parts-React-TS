818a61447fd4102a09adcf670e35eb199e87a3f4
bsao17 <declic62@gmail.com>
2022-05-20 09:58:15 +0200

    update global design

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,15 +1,19 @@
-import React, {useState, useEffect} from "react";
-import {Link, Route} from "react-router-dom";
+import React, {useState, useEffect, useContext} from "react";
+import {Link} from "react-router-dom";
+import {ThemeContext} from "../context/Context";
 import styles from "./navigation.module.scss"
-import Home from "../home/Home";
 
-export default function Navigation(){
+export default function Navigation() {
+    const context = useContext(ThemeContext)
     return (
         <div id={styles.container}>
             <ul id={styles.list}>
-                <li className={styles.itemList}><Link to={"/"}>Home</Link></li>
-                <li className={styles.itemList}><Link to={""}>About</Link></li>
-                <li className={styles.itemList}><Link to={""}>Contact</Link></li>
+                <li className={styles.itemList}><Link style={context.theme ? {color: "black"} : {color: "white"}}
+                                                      to={"/"}>Home</Link></li>
+                <li className={styles.itemList}><Link style={context.theme ? {color: "black"} : {color: "white"}}
+                                                      to={""}>About</Link></li>
+                <li className={styles.itemList}><Link style={context.theme ? {color: "black"} : {color: "white"}}
+                                                      to={""}>Contact</Link></li>
             </ul>
         </div>
     )
