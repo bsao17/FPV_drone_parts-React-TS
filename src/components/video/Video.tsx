07691d6f0094634a63c67ca8769f137c36870354
bsao17 <declic62@gmail.com>
2022-05-23 08:02:51 +0200

    merge refacto to master

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,9 +1,6 @@
 import React, {useContext} from "react";
 import styles from "./video.module.scss"
-import Home from "../home/Home";
-import DarkButton from "../darkButton/DarkButton";
 import {DarkModeContext} from "../context/Context";
-import styled from "styled-components";
 import HeaderBanner from "../header/HeaderBanner";
 
 const Video = () => {
