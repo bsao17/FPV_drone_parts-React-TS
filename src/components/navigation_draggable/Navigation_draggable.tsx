bf9458824f0fd691deb422ff970cb14e18296804
bsao17 <declic62@gmail.com>
2022-05-24 12:54:31 +0200

    Add ref and implement draggable function

--- src/components/navigation_draggable/Navigation_draggable.tsx
+++ src/components/navigation_draggable/Navigation_draggable.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import styles from "./navigation_draggable.module.scss"
 import {Link} from "react-router-dom";
 
@@ -8,13 +8,21 @@ interface mousePosition {
 }
 
 export default function Navigation_draggable(){
+    const ref = useRef<HTMLDivElement>(null)
     const [mousePos, setMousePoq] = useState<mousePosition>({
         posX: "10px",
         posY: "10px"
     })
-    console.log({"position de la souris":typeof mousePos})
+
+    const toDragWindow = () => {
+        // @ts-ignore
+        ref.current.style.top = mousePos.posY;
+        // @ts-ignore
+        ref.current.style.right = mousePos.posX;
+    }
+
     return (
-        <div className={styles.container} draggable={true}>
+        <div ref={ref} className={styles.container} onMouseDown={toDragWindow} draggable={true}>
             <ul className={styles.list}>
                 <li className={styles.listItem}><Link to={"/"}>Home</Link></li>
                 <li className={styles.listItem}><Link to={"/video"}>Video</Link></li>
