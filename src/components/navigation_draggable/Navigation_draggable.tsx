3805f94715f10aaccebedee2c4b3b9b4259c0656
bsao17 <declic62@gmail.com>
2022-05-24 11:28:24 +0200

    update Navigation_draggable.tsx

--- src/components/navigation_draggable/Navigation_draggable.tsx
+++ src/components/navigation_draggable/Navigation_draggable.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./navigation_draggable.module.scss"
 import {Link} from "react-router-dom";
 
+interface mousePosition {
+    posX: String,
+    posY: String
+}
+
 export default function Navigation_draggable(){
+    const [mousePos, setMousePoq] = useState<mousePosition>({
+        posX: "10px",
+        posY: "10px"
+    })
+    console.log({"position de la souris":typeof mousePos})
     return (
-        <div className={styles.container}>
+        <div className={styles.container} draggable={true}>
             <ul className={styles.list}>
                 <li className={styles.listItem}><Link to={"/"}>Home</Link></li>
                 <li className={styles.listItem}><Link to={"/video"}>Video</Link></li>
--- src/index.tsx
+++ src/index.tsx
@@ -11,4 +11,4 @@ root.render(
     </Context>
 );
 
-reportWebVitals(console.log);
+reportWebVitals();
