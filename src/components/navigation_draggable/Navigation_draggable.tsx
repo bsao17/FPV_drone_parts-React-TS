7851eb8094bbc8dd8832c5b5dbb08d877c19b2ef
bsao17 <declic62@gmail.com>
2022-05-25 19:35:14 +0200

    Keep it simple, stupid, you just needed React-draggable library 😒😒😒

--- src/components/navigation_draggable/Navigation_draggable.tsx
+++ src/components/navigation_draggable/Navigation_draggable.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef} from "react";
 import styles from "./navigation_draggable.module.scss"
 import {Link} from "react-router-dom";
+import Draggable from 'react-draggable';
 
 interface props {
     posX: string,
@@ -8,7 +9,11 @@ interface props {
 }
 
 export default function Navigation_draggable({posX, posY}: props) {
-    const ref = useRef<HTMLDivElement>(null)
+    const ref = useRef<HTMLUListElement>(null)
+
+    function drag_handle(e){
+        e.dataTransfer.setData("text/plain", e.target.ref)
+    }
 
     useEffect(() => {
         ref.current.style.top = posY
@@ -16,12 +21,12 @@ export default function Navigation_draggable({posX, posY}: props) {
     }, [])
 
     return (
-        <div ref={ref} className={styles.container} draggable={true}>
-            <ul className={styles.list}>
+        <Draggable>
+            <ul className={styles.list} ref={ref} onDragStart={drag_handle} draggable={"true"}>
                 <li className={styles.listItem}><Link to={"/"}>Home</Link></li>
                 <li className={styles.listItem}><Link to={"/video"}>Video</Link></li>
                 <li className={styles.listItem}><Link to={"/admin"}>Admin</Link></li>
             </ul>
-        </div>
+        </Draggable>
     )
 }
